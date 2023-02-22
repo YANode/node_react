ea728d45ca5d5e96612951b50a235f91b276f943
ANode <anode@gmail.com>
2023-02-22 12:24:41 +0200

    ls-2 created data models and the links between them

--- server/index.js
+++ server/index.js
@@ -7,12 +7,16 @@ const app = express();
 //connect sequelize
 const sequelize = require('./db');
 
+//import models
+const models = require('./models/models');
+
 const PORT = process.env.PORT || 5000;
 
+//function to connect to the database
 const start = async () => {
     try {
-        await sequelize.authenticate();
-        await sequelize.sync();
+        await sequelize.authenticate(); //establishes a connection to the database
+        await sequelize.sync(); //checks the status of the database against the data schema
         app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
     } catch (e) {
         console.log(e)
