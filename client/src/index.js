54059b1da1c92229d001376c8e836b028864bd5e
ANode <anode@gmail.com>
2023-03-04 22:09:54 +0200

    ls-12 the beginning, the structure of the application

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './other_files/index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './other_files/reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
